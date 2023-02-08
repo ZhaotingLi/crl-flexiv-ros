8d36f7eda82a81bdff26c96211c442d51653714c
zhaotli <zhaotli@student.ethz.ch>
2023-02-08 15:47:59 +0100

    add position control, tested on real flexiv robot

--- flexiv_example_controllers/src/joint_position_example_controller.cpp
+++ flexiv_example_controllers/src/joint_position_example_controller.cpp
@@ -41,7 +41,7 @@ bool JointPositionExampleController::init(hardware_interface::RobotHW* robot_har
     }
   }
 
-  std::array<double, 7> q_start{{0, -M_PI_4, 0, -3 * M_PI_4, 0, M_PI_2, M_PI_4}};
+  std::array<double, 7> q_start{{0, -0.69, 0, 1.57, 0, 0.69, 0}};
   // std::array<double, 7> q_start{{0, -M_PI_4, 0.25 * M_PI_4, -3 * M_PI_4, 0, M_PI_2, M_PI_4}};
   for (size_t i = 0; i < q_start.size(); i++) {
     std::cout<< i << " position_joint_handles_: " << position_joint_handles_[i].getPosition() <<std::endl;
@@ -67,16 +67,50 @@ void JointPositionExampleController::starting(const ros::Time& /* time */) {
 
 void JointPositionExampleController::update(const ros::Time& /*time*/,
                                             const ros::Duration& period) {
-  // elapsed_time_ += period;
 
-  // double delta_angle = M_PI / 8 * (1 - std::cos(M_PI / 5.0 * elapsed_time_.toSec())) * 0.2;
-  // for (size_t i = 0; i < 7; ++i) {
-  //   if (i == 4) {
-  //     position_joint_handles_[i].setCommand(initial_pose_[i] - delta_angle);
-  //   } else {
-  //     position_joint_handles_[i].setCommand(initial_pose_[i] + delta_angle);
-  //   }
-  // }
+    //   double delta_angle = M_PI / 8 * (1 - std::cos(M_PI / 5.0 * elapsed_time_.toSec())) * 0.2;
+
+    elapsed_time_ += period;
+
+    double magnitude_delta = 0.2;  // {0.2, 0.45, 0.6 }analyse the relationship between the discrepency of the external torque and dq
+    double delta_angle = M_PI / 16 * (1 - std::cos(M_PI / 5.0 * elapsed_time_.toSec())) * magnitude_delta;
+
+
+    double time_now = 1 / 2.0 * elapsed_time_.toSec();
+    double time_0_to_12 = int(time_now) % 12 + time_now - int(time_now) ;
+    if(time_0_to_12 < 1){
+    delta_angle = M_PI / 16 * (1 - std::cos(M_PI * time_now)) * magnitude_delta;
+    }else if(time_0_to_12 < 2){
+    delta_angle = M_PI / 8 * magnitude_delta;
+    }else if(time_0_to_12 < 3){
+    delta_angle = M_PI / 8 * magnitude_delta + M_PI / 16 * (1 - std::cos(M_PI * (time_now-2))) * magnitude_delta;
+    }else if(time_0_to_12 < 4){
+    delta_angle = M_PI / 4 * magnitude_delta;
+    }else if(time_0_to_12 < 5){
+    delta_angle = M_PI / 4 * magnitude_delta + M_PI / 16 * (1 - std::cos(M_PI * (time_now - 4))) * magnitude_delta;
+    }else if(time_0_to_12 < 6){
+    delta_angle = M_PI * 3 / 8 * magnitude_delta;
+    }else if(time_0_to_12 < 7){
+    delta_angle = M_PI / 4 * magnitude_delta + M_PI / 16 * (1 - std::cos(M_PI * (time_now - 5))) * magnitude_delta;
+    }else if(time_0_to_12 < 8){
+    delta_angle = M_PI * 2 / 8 * magnitude_delta;
+    }else if(time_0_to_12 < 9){
+    delta_angle = M_PI / 8 * magnitude_delta + M_PI / 16 * (1 - std::cos(M_PI * (time_now - 7))) * magnitude_delta;
+    }else if(time_0_to_12 < 10){
+    delta_angle = M_PI * 1 / 8 * magnitude_delta;
+    }else if(time_0_to_12 < 11){
+    delta_angle = M_PI / 16 * (1 - std::cos(M_PI * (time_now - 9))) * magnitude_delta;
+    }else{
+    delta_angle = 0;
+    }
+    
+    for (size_t i = 0; i < 7; ++i) {
+        if (i == 4) {
+        position_joint_handles_[i].setCommand(initial_pose_[i] - delta_angle);
+        } else {
+        position_joint_handles_[i].setCommand(initial_pose_[i] + delta_angle);
+        }
+    }
 
   // stay static so that it's easier to find the external torque
   // for (size_t i = 0; i < 7; ++i) {
